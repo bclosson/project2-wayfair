fd126d33bcba9e785da71f31dd26ab015967d11f
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-11-06 08:55:35 -0800

    commit added secret key back in

--- projectwayfarer_project/settings.py
+++ projectwayfarer_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+SECRET_KEY = '_^(p)rckb@n$#2iu#)8erfa8pq)ks*e*i@zfa(&xzd4mr*@uei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
